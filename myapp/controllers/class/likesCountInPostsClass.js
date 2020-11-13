6b5401c01cf92745274a4950145a00cde697df68
yoshinnu <yoshiny0610@gmail.com>
2020-11-13 13:33:45 +0900

    +clickevent hearticon

--- myapp/controllers/class/likesCountInPostsClass.js
+++ myapp/controllers/class/likesCountInPostsClass.js
@@ -4,24 +4,16 @@ module.exports =
     formatPostsData(likes, posts, user) {
       const formatPosts = [];
       posts.forEach(post => {
-        let count = 0;
-        let userLike = 'off'
-        likes.forEach(like => {
-          if (like.post_id === post.id) {
-            count++;
-            if (like.user_id === user.id) {
-              userLike = 'on';
-            }
-          }
-        })
-        let postData = {
+        const userLike = likes.find(like => like.post_id === post.id && like.user_id === user.id) ? true : false;
+        const count = likes.filter(like => like.post_id === post.id);
+        const postData = {
           id: post.id,
           title: post.title,
           text: post.text,
           userId: post.user_id,
           auther: post.auther,
-          likeCount: count,
-          userLike: userLike
+          likeCount: count.length,
+          userLike
         }
         formatPosts.push(postData);
       })
--- myapp/public/javascripts/index.js
+++ myapp/public/javascripts/index.js
@@ -2,15 +2,18 @@ const url = 'http://localhost:3000/index/post';
 const index = document.getElementById("index");
 const edit = '/edit';
 const truncate = '/edit/delete';
-const chekedHeart = '♥';
-const heart = '♡';
+const emptyHeart = 'far fa-heart';
+const checkedHeart = 'fas fa-heart';
+let posts;
+let user;
 //画面に表示させる記事作成
 window.addEventListener('load', function () {
   fetch(url)
     .then(res => res.json())
     .then(body => {
-      const posts = body.postData;
-      const user = body.user;
+      posts = body.postData;
+      user = body.user;
+      let i = 0;
       posts.forEach(post => {
         //要素の定義
         const div = document.createElement('div');
@@ -26,8 +29,9 @@ window.addEventListener('load', function () {
         const deleteForm = document.createElement('form');
         const like = document.createElement('a');
         const likeCount = document.createElement('p');
+        const heart = document.createElement('i');
         //作成開始
-        container.classList.add('container');
+        container.classList.add('listbox');
         index.appendChild(container);
         postTitle.textContent = post.title;
         container.appendChild(postTitle);
@@ -37,21 +41,26 @@ window.addEventListener('load', function () {
         container.appendChild(div);
         auther.textContent = '投稿者: ' + post.auther;
         div.appendChild(auther);
-        like.value = post.id;
+        like.value = i;
         like.href = '#';
+        like.id = 'like' + i;
         like.style.textDecoration = 'none';
         like.style.display = 'inline-block';
-        if (post.userLike === 'on') {
-          like.textContent = chekedHeart;
-          like.onclick = function () { likeCheckDelete(post, like, likeCount); };
-        } else {
-          like.textContent = heart;
-          like.onclick = function () { likeCheck(post, like, likeCount); };
-        }
         div.appendChild(like);
         likeCount.textContent = post.likeCount;
         likeCount.style.display = 'inline-block';
+        likeCount.id = 'count' + i;
         div.appendChild(likeCount);
+        heart.id = 'heart' + i;
+        like.appendChild(heart);
+        if (post.userLike) {
+          heart.className = checkedHeart;
+          like.addEventListener('click', likeCheckDelete);
+        } else {
+          heart.className = emptyHeart;
+          like.addEventListener('click', likeCheck);
+        }
+
         //userの投稿の場合更新と削除ボタンを追加
         if (post.userId === user.id) {
           updateForm.action = edit;
@@ -77,40 +86,48 @@ window.addEventListener('load', function () {
           deleteForm.appendChild(hiddenDelete);
           deleteForm.appendChild(deleteInput);
         }
+        i++;
       });
     });
 });
-function likeCheck(post, like, count) {
-  const postId = { postId: post.id };
-  const method = "POST";
-  const body = Object.keys(postId).map((key) => key + "=" + encodeURIComponent(postId[key])).join("&");
+function likeCheck() {
+  const num = this.value;
+  const heart = document.getElementById('heart' + num);
+  const postId = posts[num].id;
+  const count = document.getElementById('count' + num);
+  const postIdObj = { postId };
+  const method = 'POST';
+  const body = Object.keys(postIdObj).map((key) => key + '=' + encodeURIComponent(postIdObj[key])).join('&');
+  this.removeEventListener('click', likeCheck);
   const headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
   };
-  like.textContent = chekedHeart;
-  like.onclick = '';
+  heart.className = checkedHeart;
   count.textContent++;
   fetch("/index/likecheck", { method, headers, body })
     .then(res => {
-      like.onclick = function () { likeCheckDelete(post, like, count); };
+      this.addEventListener('click', likeCheckDelete);
     })
     .catch(console.error);
 }
-function likeCheckDelete(post, like, count) {
-  const postId = { postId: post.id };
-  const method = "POST";
-  const body = Object.keys(postId).map((key) => key + "=" + encodeURIComponent(postId[key])).join("&");
+function likeCheckDelete() {
+  const num = this.value;
+  const heart = document.getElementById('heart' + num);
+  const postId = posts[num].id;
+  const count = document.getElementById('count' + num);
+  const postIdObj = { postId };
+  const method = 'POST';
+  const body = Object.keys(postIdObj).map((key) => key + '=' + encodeURIComponent(postIdObj[key])).join('&');
   const headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
   };
-  like.textContent = heart;
-  like.onclick = '';
+  heart.className = emptyHeart;
+  this.removeEventListener('click', likeCheckDelete);
   count.textContent--;
-  fetch("/index/deletelike", { method, headers, body })
+  fetch('/index/deletelike', { method, headers, body })
     .then(res => {
-      like.onclick = function () { likeCheck(post, like, count); };
-    })
-    .catch(console.error);
+      this.addEventListener('click', likeCheck);
+    }).catch(console.error);
 }
